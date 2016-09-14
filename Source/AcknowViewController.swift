e1d15173191a6c7ccf40d9e83427877a8f7a4488
Luke47 <lukas.gauster@gmail.com>
2016-09-14 19:16:00 +0200

    updated to swift 3.0

--- Source/AcknowViewController.swift
+++ Source/AcknowViewController.swift
@@ -25,68 +25,68 @@ import UIKit
 
 /**
  Subclass of `UIViewController` that displays a single acknowledgement.
-*/
+ */
 public class AcknowViewController: UIViewController {
-
+    
     /**
-    The main text view.
-    */
+     The main text view.
+     */
     public var textView: UITextView?
-
+    
     /**
-    The represented acknowledgement.
-    */
+     The represented acknowledgement.
+     */
     var acknowledgement: Acknow?
-
+    
     /**
-    Initializes the `AcknowViewController` instance with an acknowledgement.
-
-    - parameter acknowledgement: The represented acknowledgement.
-
-    - returns: The new `AcknowViewController` instance.
-    */
+     Initializes the `AcknowViewController` instance with an acknowledgement.
+     
+     - parameter acknowledgement: The represented acknowledgement.
+     
+     - returns: The new `AcknowViewController` instance.
+     */
     public init(acknowledgement: Acknow) {
         super.init(nibName: nil, bundle: nil)
-
+        
         self.title = acknowledgement.title
         self.acknowledgement = acknowledgement
     }
-
+    
     /**
-    Initializes the `AcknowViewController` instance with a coder.
-
-    - parameter aDecoder: The archive coder.
-
-    - returns: The new `AcknowViewController` instance.
-    */
+     Initializes the `AcknowViewController` instance with a coder.
+     
+     - parameter aDecoder: The archive coder.
+     
+     - returns: The new `AcknowViewController` instance.
+     */
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.acknowledgement = Acknow(title: "", text: "")
     }
-
-
+    
+    
     // MARK: - View lifecycle
-
+    
     override public func loadView() {
-        let textView = UITextView(frame: CGRectZero)
+        let textView = UITextView(frame: CGRect.zero)
         textView.alwaysBounceVertical = true;
-        textView.font                 = UIFont.systemFontOfSize(17)
-        textView.editable             = false
-        textView.dataDetectorTypes    = UIDataDetectorTypes.Link
+        textView.font                 = UIFont.systemFont(ofSize: 17)
+        textView.isEditable             = false
+        textView.dataDetectorTypes    = UIDataDetectorTypes.link
         textView.textContainerInset   = UIEdgeInsetsMake(12, 10, 12, 10)
-
+        
         if let acknowledgement = self.acknowledgement {
             textView.text = acknowledgement.text
         }
-
+        
         self.view = textView
         self.textView = textView
     }
-
-    public override func viewWillAppear(animated: Bool) {
+    
+    public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if let textView = self.textView {
-            textView.contentOffset = CGPointMake(textView.contentInset.top, 0)
+            textView.contentOffset = CGPoint(x: textView.contentInset.top, y: 0)
         }
     }
 }
